bf39ab75386a4f612f3e13ffcf697d8e26db77eb
MwitaBrian <brianmwita13@gmail.com>
2022-09-02 23:37:36 +0300

    Pass data to html page using querySelector & added styling

--- script.js
+++ script.js
@@ -16,6 +16,12 @@ let weather = {
     const { icon, description} = data.weather[0];
     const { temp, humidity} = data.main;
     const { speed} = data.wind;
-    console.log(name, icon, description, temp, humidity, speed)
+    console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector('.city').innerText = `Weather in ${name}`;
+    document.querySelector('.weatherIcon').src = `https://openweathermap.org/img/wn/${icon}.png`;
+    document.querySelector('.description').innerText = description;
+    document.querySelector('.temp').innerText = `${temp}°C`;
+    document.querySelector('.humidity').innerText = `Humidity: ${humidity}%`;
+    document.querySelector('.wind').innerText = `Wind speed: ${speed}km/h`
   }
 }
