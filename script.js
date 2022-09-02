509751db97fe5e8c2827299bfd8f4bc9b8ebb74f
MwitaBrian <brianmwita13@gmail.com>
2022-09-02 22:18:07 +0300

    Add function to display weather data

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 let weather = {
   apiKey: '285dc7d12b49e28ef136e0bbb52e464c',
-  getWeather: function () {
-    fetch('https://api.openweathermap.org/geo/1.0/direct?q=Tokyo&units=metric&appid=285dc7d12b49e28ef136e0bbb52e464c')
+  getWeather: function getWeather (cityName) {
+    fetch(
+      'https://api.openweathermap.org/data/2.5/weather?q=' 
+      + cityName 
+      + '&units=metric&appid=' 
+      + this.apiKey
+    )
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((data) => this.displayWeather(data))
+    
+  },
+  displayWeather: function(data) {
+    const {name} = data
+    const { icon, description} = data.weather[0];
+    const { temp, humidity} = data.main;
+    const { speed} = data.wind;
+    console.log(name, icon, description, temp, humidity, speed)
   }
 }
