177397ecd7ade7eb64a4aa2208fcefbeea946533
MwitaBrian <brianmwita13@gmail.com>
2022-09-04 00:31:29 +0300

    Final edits + load new image on every search

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-  console.log('DOM fully loaded and parsed');
   let weather = {
   apiKey: '285dc7d12b49e28ef136e0bbb52e464c',
   getWeather: function getWeather (cityName) {
@@ -18,7 +17,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const { icon, description} = data.weather[0];
     const { temp, humidity} = data.main;
     const { speed} = data.wind;
-    console.log(name, icon, description, temp, humidity, speed);
     document.querySelector('.city').innerText = `Weather in ${name}`;
     document.querySelector('.weatherIcon').src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
     document.querySelector('.description').innerText = description;
@@ -26,7 +24,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     document.querySelector('.humidity').innerText = `Humidity: ${humidity}%`;
     document.querySelector('.wind').innerText = `Wind speed: ${speed}km/h`;
     document.querySelector('.weather').classList.remove("loadWindow");
-    document.body.style.backgroundImage = `url('https://source.unsplash.com/random?${name}')`
+    document.body.style.backgroundImage = `url('https://source.unsplash.com/random?${name}?sig=${Math.random()}')`;
   },
   checkWeather: function() {
     this.getWeather(document.querySelector('.search-bar').value);
